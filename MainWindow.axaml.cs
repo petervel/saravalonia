2840d271641983fd151a7c3699f9a81925ff766e
Peter Vel <petervel@gmail.com>
2026-04-01 16:17:45 +0200

    Removed the hello sarah text

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -16,8 +16,6 @@ public partial class MainWindow : Window
 
         _game = new Game(DrawCanvas);
 
-        Console.WriteLine("Hallo Sarah");
-
         _timer = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(1000 / 30) };
         _timer.Tick += OnTick!;
         _timer.Start();
